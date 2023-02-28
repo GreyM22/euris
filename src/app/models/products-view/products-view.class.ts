aba498b5e35e7e033c01f9bcd7001dfce24e9b3e
Grei <grei@linnkr.com>
2023-02-28 20:26:17 +0100

    - removed the slide out animation because was causing problems

--- src/app/models/products-view/products-view.class.ts
+++ src/app/models/products-view/products-view.class.ts
@@ -2,20 +2,10 @@ import {Component, Directive, EventEmitter, Input, Output} from "@angular/core";
 import {Product} from "../../interfaces/product.interface";
 import {animate, style, transition, trigger} from "@angular/animations";
 
-@Component({
-  template: '',
-  animations: [
-    trigger('slideOut', [
-      transition(':leave', [
-        animate('0.3s', style({transform: 'translateX(100%)', opacity: 0}))
-      ])
-    ]),
-  ]
-})
+@Directive()
 export class ProductsView {
   @Input() products!: Product[];
   @Input() displayedColumns!: (keyof Product)[];
-  @Input() animation = true;
   @Output() viewProduct = new EventEmitter<string>();
   @Output() deleteProduct = new EventEmitter<string>();
 
